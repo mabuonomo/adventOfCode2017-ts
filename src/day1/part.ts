519ab5a950f262c1cc9c0c3390a3a7bb5c3335b9
Mario Buonomo <ma.buonomo@gmail.com>
2019-02-09 21:39:40 +0100

    improve

--- src/day1/part.ts
+++ src/day1/part.ts
@@ -5,12 +5,12 @@ var reader = rd.createInterface(fs.createReadStream("./src/day1/input.txt"))
 
 var data: string;
 reader.on("line", (l: string) => {
-    data = l;
+    data = '12131415';//l;
 })
 
 reader.on("close", () => {
-    let value = main1();
-    console.log('Result 1: ' + value);
+    console.log('Result 1: ' + main1());
+    console.log('Result 2: ' + main2());
 })
 
 function main1(): number {
@@ -23,6 +23,25 @@ function main1(): number {
         } else {
             next = parseInt(data[i + 1]);
         }
+
+        if (actual === next) {
+            sum += actual;
+        }
+    }
+
+    return sum;
+}
+
+function main2(): number {
+    let sum = 0;
+    let center = data.length / 2;
+
+    for (let i = 0; i < data.length; i++) {
+        let actual = parseInt(data[i]);
+
+        let fullI = i + center;
+        let indexNext = i < center ? fullI : data.length - fullI
+        let next = parseInt(data[indexNext]);
 
         if (actual === next) {
             sum += actual;
