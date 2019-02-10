651565877f9021042eb95324652f7cf63caafe6f
Mario Buonomo <ma.buonomo@gmail.com>
2019-02-10 19:40:56 +0100

    test

--- src/day3/part.ts
+++ src/day3/part.ts
@@ -8,7 +8,7 @@ let result_matrix = matrix(size, Step.CENTER, { x: 0, y: 0 }, 1)
 // printMatrix(matrix, size * 2, true)
 
 console.log('Result 1: ' + main1());
-// // console.log('Result 2: ' + main2());
+console.log('Result 2: ' + main2(size/2));
 
 function main1() {
     let door = indexOf2d(result_matrix, 1)
@@ -19,4 +19,30 @@ function main1() {
     let distance = manhattanDistance({ x: door[0], y: door[1], z: 0 }, { x: last[0], y: last[1], z: 0 })
 
     return distance
+}
+
+function main2(value: number) {
+    let point = indexOf2d(result_matrix, value)
+    let x = point[0]
+    let y = point[1]
+
+    let sum = value;
+    try {
+        sum += result_matrix[x - 1][y] < value ? result_matrix[x - 1][y] : 0
+    } catch (e) { }
+    try {
+        sum += result_matrix[x + 1][y] < value ? result_matrix[x + 1][y] : 0
+    } catch (e) { }
+    try {
+        sum += result_matrix[x][y + 1] < value ? result_matrix[x][y + 1] : 0
+    } catch (e) { }
+    try {
+        sum += result_matrix[x][y - 1] < value ? result_matrix[x][y - 1] : 0
+    } catch (e) { }
+
+    if (sum < size) {
+        main2(++value)
+    }
+
+    return sum
 }
