94268a1aae5749d4b0492dfb898f3d2e3e085e70
Mario Buonomo <ma.buonomo@gmail.com>
2019-02-10 14:50:19 +0100

    day 2 ok

--- src/day2/part.ts
+++ src/day2/part.ts
@@ -17,7 +17,7 @@ reader.on("line", (l: string) => {
 
 reader.on("close", () => {
     console.log('Result 1: ' + main1());
-    // console.log('Result 2: ' + main2());
+    console.log('Result 2: ' + main2());
 })
 
 function main1(): number {
@@ -27,6 +27,28 @@ function main1(): number {
         let min = Math.min(...row);
 
         sum += max - min;
+    });
+
+    return sum;
+}
+
+function main2(): number {
+    let sum = 0;
+    data.forEach(row => {
+        let values = [];
+        row.forEach(element => {
+
+            // found two numbers in mod 0
+            let found = row.some( // at least once
+                (value, index, array) =>
+                    element != value && Math.max(element, value) % Math.min(element, value) == 0)
+
+            if (found) {
+                values.push(element)
+            }
+        });
+
+        sum += Math.max(...values) / Math.min(...values)
     });
 
     return sum;
